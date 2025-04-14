7783d470fb509512fee8b2316ccc07a694783822
Matan Benoon <matanbn1121@gmail.com>
2025-04-14 03:33:56 -0700

    .

--- sql-project/public/src/components/Button.tsx
+++ sql-project/public/src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '../components/button.module.scss';
+import styles from '../../src/components/Button.module.scss';
 
 interface Props {
   text: string;
