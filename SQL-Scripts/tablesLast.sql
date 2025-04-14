bc93894b933e1783eec78c2ad9d3abf74ecddaee
Yosef Ibrahim <yosefib88@gmail.com>
2025-04-15 00:30:00 +0300

    finished the server-side

---
+++ SQL-Scripts/tablesLast.sql
@@ -0,0 +1,117 @@
+CREATE DATABASE IF NOT EXISTS SQL_slr_Project;
+use sql_slr_Project;
+
+-- Client Table
+create table clients (
+client_id int not null auto_increment primary key,
+client_name varchar(200),
+client_entry_date date,
+client_address_id int,
+foreign key(client_address_id) references address(address_id) on delete cascade,
+client_phone varchar(50),
+client_email VARCHAR(100) UNIQUE not null,
+client_password varchar(50),
+client_company_registration date
+);
+
+insert into clients (client_firstName, client_email, client_password) values
+('dalton','dalton@gmail.com','123');
+
+select * from clients;
+
+alter table clients
+add column client_address varchar(200);
+
+ALTER TABLE clients
+DROP COLUMN client_user;
+
+
+-- Address Table
+create table address(
+address_id int not null auto_increment primary key,
+city varchar(50),
+street varchar(100),
+building_number int,
+postal_code int,
+additional_info varchar(200),
+country varchar(100)
+);
+
+-- Knives Table
+create table knives (
+knives_id int not null auto_increment primary key,
+knives_dimensions varchar(50),
+knives_stock boolean,
+need_to_order boolean,
+sent_for_repair boolean
+);
+
+
+-- Engravings Table
+create table engravings (
+engravings_id int not null auto_increment primary key,
+engravings_name varchar(50) unique not null,
+engravings_description varchar(100)
+);
+
+-- Materials Table
+create table materials (
+materials_id int not null auto_increment primary key,
+material_description varchar(200),
+material_width int
+);
+
+insert into materials (material_description, material_width) values
+('Sable', '250');
+
+select * from materials;
+
+
+create table sticker (
+sticker_id int not null auto_increment primary key,
+client_id int,
+sticker_name varchar(100),
+sticker_price int,
+order_id int,
+foreign key(client_id) references clients(client_id) on delete cascade,
+foreign key(order_id) references orders(order_id) on delete cascade
+);
+
+
+CREATE TABLE orders(
+order_id int not null auto_increment primary key,
+order_date DATE,
+delivery_date DATE,
+order_arrival_date DATE,
+order_feedback VARCHAR(30),
+order_sticker_quantity INT,
+client_id INT,
+knives_id INT,
+engravings_id INT,
+materials_id INT,
+FOREIGN KEY (client_id) REFERENCES clients(client_id),
+FOREIGN KEY (knives_id) REFERENCES knives(knives_id),
+FOREIGN KEY (engravings_id) REFERENCES engravings(engravings_id),
+FOREIGN KEY (materials_id) REFERENCES materials(materials_id)
+);
+
+alter table orders
+add column sticker_id int;
+
+alter table orders
+ADD FOREIGN KEY (sticker_id) REFERENCES sticker(sticker_id);
+
+select * from clients;
+
+
+
+
+
+
+
+
+
+
+
+ 
+
--- sql-project/server/src/server.ts
+++ sql-project/server/src/server.ts
@@ -1,13 +1,91 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import mysql from 'mysql2/promise';
+import dotenv from "dotenv";
+import cors from 'cors';
+import mainRoutes from './Routes/mainRoutes';
+export type RequestHandler = (req: Request, res: Response) => Promise<void> | void;
+dotenv.config()
 
+// Create the Express application
 const app = express();
+const PORT = process.env.PORT || 3000;
+const myPassword = process.env.myPassword;
 
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176'], // Array of allowed origins
+    credentials: true,
+}));
+
+// Configure middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Server is running on port 3000');
+// Create MySQL connection pool
+export const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: myPassword,
+    database: 'sql_slr_Project',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
+
+const AddProduct: RequestHandler = async (req, res) => {
+    try{
+        const { products_name, products_price, products_description, products_imageUrl} = req.body;
+        console.log(products_name)
+        console.log(products_price)
+        console.log(products_description)
+        console.log(products_imageUrl)
+
+        if (!products_description || !products_imageUrl || !products_name || !products_price)
+        {
+            res.status(400).json({ message: 'Data are required' });
+            return;
+        }
+
+         // Insert new product to the database
+         const [result] = await pool.execute(
+            'insert into products (products_name, products_price, products_description, products_imageUrl) values (?, ?, ?, ?)',
+            [products_name,products_price, products_description, products_imageUrl]
+        );
+
+        console.log("results", result);
+
+        res.status(200).json({
+            success: true,
+            message: 'added product successful!',
+        });
+
+
+    }
+    catch (error) {
+        console.error('insert new products error:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Error add products',
+            error: (error as Error).message
+        });
+    }
+}
+
+app.post('/api/products/addProduct', AddProduct);
+app.use("/api", mainRoutes);
+
+// Start the server
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+            console.log('SLR System is start working');
+
+        });
+    } catch (error) {
+        console.error('Server startup failed:', error);
+        process.exit(1);
+    }
+};
+
+startServer();
