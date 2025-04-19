9a9a52e4c78e27a2d38b949f5e8f5aaa3473f7d4
Yosef Ibrahim <yosefib88@gmail.com>
2025-04-20 01:16:14 +0300

    user can see his orders

--- sql-project/server/src/controllers/fetchOrdersByClient.ts
+++ sql-project/server/src/controllers/fetchOrdersByClient.ts
@@ -1,23 +1,35 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const secret = process.env.JWT_SECRET;
+
 import { RequestHandler } from "express";
-import { pool } from "../models/db"
+import { pool } from "../models/db";
+import jwt from 'jwt-simple';
 
 export const fetchOrdersByClient: RequestHandler = async (req, res) => {
     try{
+                const user = req.cookies.token; 
+                
+                const decoded = jwt.decode(user, secret as string);
+                const clientId = decoded.client_id;
          const [result] = await pool.execute(
             `SELECT
                 c.client_id,
                 o.order_id,
                 o.order_date,
                 o.delivery_date,
-                o.order_feedback,
-                o.order_sticker_quantity,
-                m.material_description
+                o.praises,
+                o.sticker_quantity,
+                m.materials_type
             FROM
                 orders o
             JOIN
                 materials m ON m.materials_id = o.materials_id
             JOIN
-                clients c ON c.client_id = o.client_id;`,
+                clients c ON c.client_id = o.client_id
+            WHERE
+                c.client_id = ?;`,[clientId]
         );
 
         // console.log("results", result);
