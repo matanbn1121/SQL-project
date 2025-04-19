fbf0450882633a60e6c7492d512f6e3fe6f6cc25
Yosef Ibrahim <yosefib88@gmail.com>
2025-04-20 01:17:32 +0300

    1

--- sql-project/server/src/controllers/fetchOrdersByClient.ts
+++ sql-project/server/src/controllers/fetchOrdersByClient.ts
@@ -1,35 +1,23 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const secret = process.env.JWT_SECRET;
-
 import { RequestHandler } from "express";
-import { pool } from "../models/db";
-import jwt from 'jwt-simple';
+import { pool } from "../models/db"
 
 export const fetchOrdersByClient: RequestHandler = async (req, res) => {
     try{
-                const user = req.cookies.token; 
-                
-                const decoded = jwt.decode(user, secret as string);
-                const clientId = decoded.client_id;
          const [result] = await pool.execute(
             `SELECT
                 c.client_id,
                 o.order_id,
                 o.order_date,
                 o.delivery_date,
-                o.praises,
-                o.sticker_quantity,
-                m.materials_type
+                o.order_feedback,
+                o.order_sticker_quantity,
+                m.material_description
             FROM
                 orders o
             JOIN
                 materials m ON m.materials_id = o.materials_id
             JOIN
-                clients c ON c.client_id = o.client_id
-            WHERE
-                c.client_id = ?;`,[clientId]
+                clients c ON c.client_id = o.client_id;`,
         );
 
         // console.log("results", result);
